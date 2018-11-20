d989bff443473c96347a625be5dcd748071b7379
Gerstein Liam <liam.gerstein@gmail.com>
2018-11-20 21:42:28 +0100

    Mise en forme du code

--- BF3_SCAN/BF3_SCAN/MainWindow.xaml.cs
+++ BF3_SCAN/BF3_SCAN/MainWindow.xaml.cs
@@ -24,12 +24,10 @@ namespace BF3_SCAN
     public partial class MainWindow : Window
     {
         private Task Scan_Task;
-
         public MainWindow()
         {
             InitializeComponent();
         }
-
         private void bt_scan(object sender, RoutedEventArgs e)
         {
             if (button_scan.Content.Equals("Start scanning"))
@@ -40,59 +38,37 @@ namespace BF3_SCAN
                 string Request_Map_para = "No_set";
                 Int32 Scan_Rate_para = Int32.Parse(Scan_Rate.GetLineText(0));
                 string Server_URL_para = Server_URL.GetLineText(0);
-
-
-
-
                 if (cb_Kharg.IsChecked == true)
                 {
-
                     Request_Map_para = (string)cb_Kharg.Content;
                     Console.WriteLine(Request_Map_para);
-
                 }
-
                 if (cb_Wake.IsChecked == true)
                 {
-
                     Request_Map_para = (string)cb_Wake.Content;
                     Console.WriteLine(Request_Map_para);
-
                 }
-
                 if (cb_Shargi.IsChecked == true)
                 {
-
                     Request_Map_para = (string)cb_Shargi.Content;
                     Console.WriteLine(Request_Map_para);
-
                 }
-
                 if (cb_Noshahr.IsChecked == true)
                 {
-
                     Request_Map_para = (string)cb_Noshahr.Content;
                     Console.WriteLine(Request_Map_para);
-
                 }
-
                 SCAN_parameters sc_para = new SCAN_parameters(Server_URL_para, Scan_Rate_para, Request_Map_para);
-
-                Scan_Task = Task.Factory.StartNew(() => {
+                Scan_Task = Task.Factory.StartNew(() =>
+                {
                     GetServerInfo(sc_para);
                 });
-
-
             }
             else
             {
                 button_scan.Content = "Start scanning";
             }
-
-
-
         }
-
         private void GetServerInfo(object sc_para)
         {
             SCAN_parameters this_sc_para = (SCAN_parameters)sc_para;
@@ -144,40 +120,28 @@ namespace BF3_SCAN
                     {
                         mode_str = words[ptr];
                     }
-
-
                     else
                     {
                         ptr++;
                     }
                 }
                 //   Console.Clear();
-
-
-
-
                 Console.WriteLine("------------SCAN PARAMETER-------------------");
                 Console.WriteLine("URL             :");
                 Console.WriteLine(this_url);
                 Console.WriteLine("REQUESTED MAP   : " + this_requested_map);
                 Console.WriteLine("SCAN FREQ [s]   : " + this_serv_freq);
-
                 string last_scan = DateTime.Now.ToString();
                 string scan_duration = DateTime.Now.Subtract(RoundBeginTime).ToString();
-                Dispatcher.Invoke(() => {
-
+                Dispatcher.Invoke(() =>
+                {
                     Result_Server_name.Text = server_name[1].InnerHtml;
                     Result_running_map.Text = map_running;
                     Result_mode.Text = mode_str;
                     Result_last_scan.Text = last_scan;
                     Result_scan_duration.Text = scan_duration;
-
-
-
                     if (map_running.Contains(this_requested_map))
                     {
-
-
                         Result_matching_text.Content = this_requested_map + " now! ";
                         Result_matching_rect.Fill = new SolidColorBrush(System.Windows.Media.Colors.Green);
                         Console.WriteLine("!!!!!!!!!!Your map is running!!!!!!!       :");
@@ -185,28 +149,12 @@ namespace BF3_SCAN
                         Console.WriteLine("!!!!!!!!!!Your map is running!!!!!!!       :");
                         Console.WriteLine("!!!!!!!!!!Your map is running!!!!!!!       :");
                     }
-
                     else
                     {
-
                         Result_matching_rect.Fill = new SolidColorBrush(System.Windows.Media.Colors.Red);
-
                     }
-
-
-
-
-
                 });
-
-
-
-
-
-
                 Console.WriteLine("Server          :   " + server_name[1].InnerHtml);
-
-
                 Console.WriteLine("------------SCAN RESULTS----------------------");
                 Console.WriteLine("Last SCAN       :" + DateTime.Now);
                 Console.WriteLine("Server          :   " + server_name[1].InnerHtml);
@@ -214,19 +162,13 @@ namespace BF3_SCAN
                 Console.WriteLine("MODE            :   " + mode_str);
                 Console.WriteLine("  ");
                 Console.WriteLine("Round Duration  :" + DateTime.Now.Subtract(RoundBeginTime));
-
                 if (map_running.Contains(this_requested_map))
                 {
-
-
-
                     Console.WriteLine("!!!!!!!!!!Your map is running!!!!!!!       :");
                     Console.WriteLine("!!!!!!!!!!Your map is running!!!!!!!       :");
                     Console.WriteLine("!!!!!!!!!!Your map is running!!!!!!!       :");
                     Console.WriteLine("!!!!!!!!!!Your map is running!!!!!!!       :");
                 }
-
-
                 try
                 {
                     Thread.Sleep(1000 * this_serv_freq);
@@ -234,8 +176,6 @@ namespace BF3_SCAN
                 catch (ThreadInterruptedException) { }
             }
         }
-
-
         public class SCAN_parameters
         {
             string Server_url;
@@ -260,7 +200,5 @@ namespace BF3_SCAN
                 return Requested_Map;
             }
         }
-
-
     }
 }
